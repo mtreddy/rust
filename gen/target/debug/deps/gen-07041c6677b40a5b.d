b237b901f97133bac2ddff2150dcf5eea33b7f3a
mtreddy <mtreddy@gmail.com>
2023-05-28 16:16:37 -0700

    First rust progr

---
+++ gen/target/debug/deps/gen-07041c6677b40a5b.d
@@ -0,0 +1,5 @@
+/home/tmarri/src/git/rust/gen/target/debug/deps/gen-07041c6677b40a5b: src/main.rs
+
+/home/tmarri/src/git/rust/gen/target/debug/deps/gen-07041c6677b40a5b.d: src/main.rs
+
+src/main.rs:
---
+++ gen/target/debug/gen.d
@@ -0,0 +1,1 @@
+/home/tmarri/src/git/rust/gen/target/debug/gen: /home/tmarri/src/git/rust/gen/src/main.rs
