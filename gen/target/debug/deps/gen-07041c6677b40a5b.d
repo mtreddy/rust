fac8890dcd16c427aac8a11bd9b7b08011c80307
mtreddy <mtreddy@gmail.com>
2023-06-01 14:42:58 -0700

    Initial commit

--- gen/target/debug/deps/gen-07041c6677b40a5b.d
+++
@@ -1,5 +0,0 @@
-/home/tmarri/src/git/rust/gen/target/debug/deps/gen-07041c6677b40a5b: src/main.rs
-
-/home/tmarri/src/git/rust/gen/target/debug/deps/gen-07041c6677b40a5b.d: src/main.rs
-
-src/main.rs:
--- gen/target/debug/gen.d
+++
@@ -1,1 +0,0 @@
-/home/tmarri/src/git/rust/gen/target/debug/gen: /home/tmarri/src/git/rust/gen/src/main.rs
