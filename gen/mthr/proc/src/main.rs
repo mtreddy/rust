910db86c22efd9bace93fcb9a2f2235bead84418
mtreddy <mtreddy@gmail.com>
2024-03-15 21:20:20 -0700

    Spawn process

--- gen/mthr/proc/src/main.rs
+++ gen/mthr/proc/src/main.rs
@@ -1,3 +1,12 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let mut child = Command::new("/bin/cat")
+        .arg("file.txt")
+        .spawn()
+        .expect("failed to execute child");
+
+    let ecode = child.wait().expect("failed to wait on child");
+
+    assert!(ecode.success());
 }
